4c6e07c239ca23c837dd612187bd9e8dfdef06c2
GurunadhD <gurunadhacc@gmail.com>
2025-06-03 17:26:40 +0530

    Created ListNode & LinkedList classes.

---
+++ linked-list.ts
@@ -0,0 +1,8 @@
+// Individual node in the linked list
+class ListNode {}
+
+// Used to store the root node of the linked list
+class LinkedList {
+    private root?: ListNode;
+    private length = 0; 
+}
